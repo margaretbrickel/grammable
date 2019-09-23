012ce4564180baa7a80c9846643a03184dd79c84
Margaret Brickel <mbrickel@gmail.com>
2019-09-23 17:58:29 +0000

    Adding FactoryBot

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :user do
+    sequence (:email) { |n| "dummyEmail#{n}@gmail.com" }
+    password { "secretPassword" }
+    password_confirmation { "secretPassword" }
+  end
+end
