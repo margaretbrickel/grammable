102be3e00f88d13f6d1a91af5334d2299d73c216
Margaret Brickel <mbrickel@gmail.com>
2019-09-24 07:10:35 +0000

    Support Create Comments on Grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -60,7 +60,5 @@ end
     params.require(:gram).permit(:message, :picture)
   end
 
-  def render_not_found(status=:not_found)
-    render plain: "#{status.to_s.titleize} :(", status: status
-  end
+  
 end
--- app/models/gram.rb
+++ app/models/gram.rb
@@ -3,5 +3,6 @@ class Gram < ApplicationRecord
 
   validates :message, presence: true
   validates :picture, presence: true
+  has_many :comments
   belongs_to :user
 end
